a12a0a60984855df4c5607cb5aa402da2ec4a91e
msgggg1 <msggms1@gmail.com>
2025-07-08 23:32:54 +0900

    days07 복습

--- SL05_MYBATIS/src/main/java/org/doit/ik/domain/DeptDTO.java
+++ SL05_MYBATIS/src/main/java/org/doit/ik/domain/DeptDTO.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Alias("DeptDTOAlias")
 public class DeptDTO {
+	
 	   private int deptno;
 	   private String dname;
 	   private String loc;
